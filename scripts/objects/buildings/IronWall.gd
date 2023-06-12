070e9c8ae0006a3f37aaab66f7681b47cf3c3517
Krzysztof Staroń <kisiel3141@gmail.com>
2023-06-12 15:08:55 +0200

    Destroy Iron Wall

--- scripts/objects/buildings/IronWall.gd
+++ scripts/objects/buildings/IronWall.gd
@@ -13,5 +13,24 @@ func interact_left():
 		return
 
 	if item.type == "tool" and item.toolType == Tool.Hammer:
-		pass
+		destroy()
+		
+func destroy():
+	var tileMap : TileMap = get_parent()
+	var tilePosition : Vector2 = tileMap.world_to_map(position)
+	
+	var destroy : Node = preload("res://scenes/objects/buildings/IronWall/destroy.tscn").instance()
+	destroy.position = position
+	destroy.emitting = true
+	get_parent().add_child(destroy)
+
+	var dropScene : Node = preload("res://scenes/droppedItem.tscn").instance()
+	dropScene.position = position
+	dropScene.item = load("res://data/items/buildings/IronWall.tres")
+	
+	get_parent().add_child(dropScene)
+	tileMap.set_cellv(tilePosition, -1)
+	tileMap.update_bitmask_area(tilePosition)
+	
+	queue_free()
 
