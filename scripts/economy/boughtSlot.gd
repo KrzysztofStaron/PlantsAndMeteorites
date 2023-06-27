9dc8644e754dbc2372300ec486eb538c2fe93d7e
Krzysztof Staroń <kisiel3141@gmail.com>
2023-06-27 21:01:48 +0200

    sum and del

--- scripts/economy/boughtSlot.gd
+++ scripts/economy/boughtSlot.gd
@@ -1,9 +1,42 @@
 extends TextureRect
 
+export var index : int
+
+func _ready():
+	setItem(null)
+
 func setItem(item : InventoryItem):
 	if item == null:
 		$icon.texture = null
 		$amount.text = ""
+		$remove.hide()
+		$sub.hide()
 	else:
+		$remove.show()
+		$sub.show()
 		$icon.texture = item.texture
 		$amount.text = str(item.quantity)
+
+func sub():
+	var item : InventoryItem = $"../..".inventory[index]
+	item.quantity -= 1
+	
+	if item.quantity <= 0:
+		$"../..".inventory[index] = null
+		$icon.texture = null
+		$amount.text = ""
+		
+		$remove.hide()
+		$sub.hide()
+	else:	
+		$amount.text = str(item.quantity)
+		
+	
+
+func remove():
+	$"../..".inventory[index] = null
+	$remove.hide()
+	$sub.hide()
+	
+	$icon.texture = null
+	$amount.text = ""
